839375670f4a9da25e6b7d31952fc2f7b239f094
prateekbhamboo2003-collab <prateekbhamboo2003@gmail.com>
2025-11-23 21:41:49 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,10 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
-auth_users = [1713924419]
-sudo_user = 1713924419
+api_id = 32402877
+api_hash = "145427eddb5c1bc4dc943d0a89f0f98b"
+bot_token = "8458145401:AAGVWdj2-kWFk6oVSDrY_0IFk1XNzN5T7Fc"
+auth_users = [7268886493]
+sudo_user = 7268886493
 log_channel = -1002143629860
 txt_channel = -1002049391187
+
 
 
